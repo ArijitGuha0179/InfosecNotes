e8ce1b4bc2765e1a968f7f3e0c8c42a183ef71ac
himanshudas75 <himanshudas75@gmail.com>
2021-06-23 17:55:46 +0530

    Tesseract script added

---
+++ Scripts/Tesseract/ocr.py
@@ -0,0 +1,21 @@
+import cv2
+import pytesseract
+import os
+
+imgname=''
+imgextensions=['png','PNG','JPEG','jpeg','jpg','JPG']
+for file in os.listdir('./images/'):
+    if file[-3:] in imgextensions:
+        imgname=f'./image/{file}'
+
+img = cv2.imread(imgname)
+
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+text=pytesseract.image_to_string(img)
+text=text.replace('”','"')
+text=text.replace('“','"')
+text=text.replace("’","'")
+text=text.replace("‘","'")
+print(f'\n\n{text}\n\n')
+
+os.remove(imgname)
